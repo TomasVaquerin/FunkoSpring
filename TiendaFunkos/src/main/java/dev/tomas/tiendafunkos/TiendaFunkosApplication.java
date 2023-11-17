9f5b06aff6fa80f000bef5f1a54d10b32e403594
TomasVaquerin <83606915+CuboAzul@users.noreply.github.com>
2023-11-17 11:34:05 +0100

    ProyectoBase

---
+++ TiendaFunkos/src/main/java/dev/tomas/tiendafunkos/TiendaFunkosApplication.java
@@ -0,0 +1,13 @@
+package dev.tomas.tiendafunkos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFunkosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaFunkosApplication.class, args);
+    }
+
+}
---
+++ TiendaFunkos/src/test/java/dev/tomas/tiendafunkos/TiendaFunkosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tomas.tiendafunkos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFunkosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
